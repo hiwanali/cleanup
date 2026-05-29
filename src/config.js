3c367b604b6262ed5ea79c978fa6e0268db7e28d
Hiwan Ali <Hiwanali@hotmail.com>
2026-05-29 14:23:31 +0200

    hej

---
+++ src/config.js
@@ -0,0 +1,7 @@
+/* Manual config - används om Vercel env vars inte fungerar */
+(function () {
+  window.__CLEANUP_CONFIG__ = {
+    url: 'https://bkmnlcdsbvpucpqmaycx.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJrbW5sY2RzYnZwdWNwcW1heWN4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwMzIxNTMsImV4cCI6MjA5NTYwODE1M30.KI30f-J4OFjUTyGGlLKEX51XamjbuYRspMzvWZwFTcQ',
+  };
+})();
